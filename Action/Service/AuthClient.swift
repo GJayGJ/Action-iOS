b8e4f0eefe40e5b2dbd3e6138f2c77e5a866ac49
GJayGJ <jay.gj.huang@gmail.com>
2024-05-09 21:57:58 +0100

    Add Package Dependencies of grpc-swift into Action Target

--- Action/Service/AuthClient.swift
+++ Action/Service/AuthClient.swift
@@ -13,21 +13,21 @@ class AuthClient {
     var authServiceClient: Action_ActionWrapperNIOClient?
     let port: Int = 50051
     init() {
-      // build a fountain of EventLoops
-      let eventLoopGroup = MultiThreadedEventLoopGroup(numberOfThreads: 1)
-      do {
-        // open a channel to the gPRC server
-        let channel = try GRPCChannelPool.with(
-          target: .host("localhost", port: self.port),
-          transportSecurity: .plaintext,
-          eventLoopGroup: eventLoopGroup
-        )
-        // create a Client
-        self.authServiceClient = Action_ActionWrapperNIOClient(channel: channel)
-        print("grpc connection initialized")
-      } catch {
-        print("Couldn’t connect to gRPC server")
-      }
+        // build a fountain of EventLoops
+        let eventLoopGroup = MultiThreadedEventLoopGroup(numberOfThreads: 1)
+        do {
+            // open a channel to the gPRC server
+            let channel = try GRPCChannelPool.with(
+                target: .host("localhost", port: self.port),
+                transportSecurity: .plaintext,
+                eventLoopGroup: eventLoopGroup
+            )
+            // create a Client
+            self.authServiceClient = Action_ActionWrapperNIOClient(channel: channel)
+            print("grpc connection initialized")
+        } catch {
+            print("Couldn’t connect to gRPC server")
+        }
     }
     /**
      * Unary call example. Calls `login` and prints the response.
@@ -40,29 +40,29 @@ class AuthClient {
      * which it attempts to parse.
      */
     func login(username: String, password: String) -> String {
-      print("Login: username=\(username)")
-      // build the AccountCredentials object
-      let accountCredentials: Action_User = .with {
-        $0.userAccount = username
-        $0.userPassword = password
-      }
-      // grab the login() method from the gRPC client
-      let call = self.authServiceClient!.login(accountCredentials)
-      // prepare an empty response object
-      let oauthCredentials:  SwiftProtobuf.Google_Protobuf_StringValue
-      // execute the gRPC call and grab the result
-      do {
-        oauthCredentials = try call.response.wait()
-      } catch {
-        print("RPC method ‘login’ failed: \(error)")
-        // it would be better to throw an error here, but
-        // let’s keep this simple for demo purposes
-        return ""
-      }
-      // Do something interesting with the result
-      print(oauthCredentials)
-      // return a value so we can use it in the app
-      return "Logged in!"
+        print("Login: username=\(username)")
+        // build the AccountCredentials object
+        let accountCredentials: Action_User = .with {
+            $0.userAccount = username
+            $0.userPassword = password
+        }
+        // grab the login() method from the gRPC client
+        let call = self.authServiceClient!.login(accountCredentials)
+        // prepare an empty response object
+        let oauthCredentials:  SwiftProtobuf.Google_Protobuf_StringValue
+        // execute the gRPC call and grab the result
+        do {
+            oauthCredentials = try call.response.wait()
+        } catch {
+            print("RPC method ‘login’ failed: \(error)")
+            // it would be better to throw an error here, but
+            // let’s keep this simple for demo purposes
+            return ""
+        }
+        // Do something interesting with the result
+        print(oauthCredentials)
+        // return a value so we can use it in the app
+        return "Logged in!"
     }
-  
+    
 }
